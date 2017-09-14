62439224e3d644974c546256edd07f6c500b0ed6
Holt Wilkins <hwilkins@palantir.com>
2017-09-14 14:50:01 +1000

    Remove Circle 1.0 cd cruft

--- circleci/publish.sh
+++ circleci/publish.sh
@@ -3,9 +3,7 @@
 set -eu
 
 # Push go tgz
-pushd "$GO_PROJECT_SRC_PATH"
 ./godelw publish bintray --url https://api.bintray.com --subject palantir --repository releases --user "$BINTRAY_USERNAME" --password "$BINTRAY_PASSWORD" --publish --downloads-list duo-bot
-popd
 
 version=$(./godelw project-version)
 
